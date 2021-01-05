1f7e190209c41d5a7525b7108459cc40a6b321e7
Matt Paget <matt.paget@csiro.au>
2021-01-05 18:08:19 +1100

    argo@2.11.7

---
+++ Formula/argo@2.11.7.rb
@@ -0,0 +1,35 @@
+class ArgoAT2117 < Formula
+  desc "Get stuff done with container-native workflows for Kubernetes"
+  homepage "https://argoproj.io"
+  url "https://github.com/argoproj/argo.git",
+      tag:      "v2.11.7",
+      revision: "bf3fec176cf6bdf3e23b2cb73ec7d4e3d051ca40"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "node" => :build
+  depends_on "yarn" => :build
+
+  def install
+    # this needs to be remove to prevent multiple 'operation not permitted' errors
+    inreplace "Makefile", "CGO_ENABLED=0", ""
+    system "make", "dist/argo"
+    bin.install "dist/argo"
+
+    output = Utils.safe_popen_read("#{bin}/argo", "completion", "bash")
+    (bash_completion/"argo").write output
+    output = Utils.safe_popen_read("#{bin}/argo", "completion", "zsh")
+    (zsh_completion/"_argo").write output
+  end
+
+  test do
+    assert_match "argo is the command line interface to Argo",
+      shell_output("#{bin}/argo --help")
+
+    # argo consumes the Kubernetes configuration with the `--kubeconfig` flag
+    # Since it is an empty file we expect it to be invalid
+    touch testpath/"kubeconfig"
+    assert_match "invalid configuration",
+      shell_output("#{bin}/argo lint --kubeconfig ./kubeconfig 2>&1", 1)
+  end
+end
